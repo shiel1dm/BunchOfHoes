a0cc7260df7d24451e9ee22f30c42e1fa8979607
depotte6 <depotte6@gmail.com>
2021-09-08 13:41:39 -0400

    added more data to seed files

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
+
 app.set('view engine', 'hbs');
 app.engine('hbs', handle({
   extname: 'hbs',
