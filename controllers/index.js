da9b5dff8819b9994eb05bf28b81aa506233f036
calyun <cmyounk@gmail.com>
2021-09-11 01:48:44 -0400

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,19 +1,34 @@
 var router = require('express').Router();
-const { Model, Post, Topic, User, Reply } = require('../models');
+const {
+    Model,
+    Post,
+    Topic,
+    User,
+    Reply
+} = require('../models');
 const userRouter = require('./api/userRoutes');
 const blogRouter = require('./api/blogRoutes');
 const auth = require('./auth');
 
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
     const postData = await Post.findAll()
-    const posts = postData.map((post) => post.get({ plain: true }));
+    const posts = postData.map((post) => post.get({
+        plain: true
+    }));
     // console.log(posts)
 
     const topicData = await Topic.findAll()
-    const topics = topicData.map((topic) => topic.get({ plain: true }));
+    const topics = topicData.map((topic) => topic.get({
+        plain: true
+    }));
     console.log(topics)
 
-    res.render('home', {posts, topics, layout: 'main', view: 'home'});
+    res.render('home', {
+        posts,
+        topics,
+        layout: 'main',
+        view: 'home'
+    });
 });
 
 router.use(userRouter);
